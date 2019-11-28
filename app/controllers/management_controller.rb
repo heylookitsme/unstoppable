ecbbd52a9da92fab01bac0b0fa6f0acaf0a28130
shardax <sarada.chintala@gmail.com>
2019-11-28 10:11:03 -0500

    Add Forgot password

--- app/controllers/management_controller.rb
+++ app/controllers/management_controller.rb
@@ -7,9 +7,19 @@ class ManagementController < ApplicationController
     
     def send_username
         @user = User.find_by_email(params[:user][:email])
-        Rails.logger.debug("send_userbane @user = #{@user.inspect}")
-        UserMailer.forgot_username(@user).deliver
-        flash[:success] = "An email has been sent to your account with your username."
-        render "forward_username"
+        Rails.logger.debug("send_userbane @user = #{@user.inspect} params = #{params.inspect}")
+        if(params[:stype] == "P")
+           @user.send_reset_password_instructions
+           render "forward_password"
+        else
+            UserMailer.forgot_username(@user).deliver
+            flash[:success] = "An email has been sent to your account with your username."
+            render "forward_username"
+        end
+    end
+
+    def get_email_address
+        Rails.logger.info("In GET_EMAIL_ADDRESS")
+        render 'get_email_address'
     end
 end
