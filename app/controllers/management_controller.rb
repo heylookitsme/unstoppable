945e537de92669e122ab1a655df71685b23c4d6b
shardax <sarada.chintala@gmail.com>
2019-12-13 13:16:53 -0500

    Add invisible recaptcha for forget username and forget password

--- app/controllers/management_controller.rb
+++ app/controllers/management_controller.rb
@@ -1,13 +1,26 @@
 class ManagementController < ApplicationController
-
+    
     def get_email_address
+        flash.delete(:non_existent_user_error)
         Rails.logger.info("In GET_EMAIL_ADDRESS")
         render 'get_email_address'
     end
     
     def send_username
+        #Rails.logger.info("Insend_username params = #{params.inspect}")
         @user = User.find_by_email(params[:user][:email])
-        Rails.logger.debug("send_userbane @user = #{@user.inspect} params = #{params.inspect}")
+        #Rails.logger.info("Insend_username @user = #{@user.inspect}")
+        flash.delete(:non_existent_user_error)
+        if @user.blank?
+            if params[:user][:email].blank?
+                flash[:non_existent_user_error] = "Please enter an email."
+            else
+                flash[:non_existent_user_error] = "No User exists for this email #{params[:user][:email].inspect} Please re-enter email."
+            end
+            render 'get_email_address'
+            return
+        end
+        #Rails.logger.debug("send_userbane @user = #{@user.inspect} params = #{params.inspect}")
         if(params[:stype] == "P")
            @user.send_reset_password_instructions
            render "forward_password"
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -16,6 +16,7 @@ class UserMailer < ApplicationMailer
 
   def forgot_username(user)
     @user = user
+    @home = root_url
     mail(:to => "#{user.username} <#{user.email}>", :subject => "Username Reminder")
   end
 
