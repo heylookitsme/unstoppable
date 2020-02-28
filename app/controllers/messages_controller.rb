9d29b05330774b48fab3d5a36ad5176d38a376a0
shardax <sarada.chintala@gmail.com>
2020-02-28 14:08:04 -0500

    Add comments for heroku

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,7 +3,11 @@ class MessagesController < ApplicationController
 
     def create
         receipt = current_user.reply_to_conversation(@conversation, params[:body])
+        Rails.logger.debug "In Message Controller conversation = #{@conversation.inspect}"
+        Rails.logger.debug "In Message Controller messages = #{@conversation.messages.inspect}"
+        Rails.logger.debug "In Message Controller receipt = #{receipt.inspect}"
         redirect_to conversation_path(receipt.conversation)
+        #redirect_to conversation_path(receipt.conversation)
     end
 
     private
