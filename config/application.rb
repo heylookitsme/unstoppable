5f7560315d5397acaf14872ef12b2ee4c9f9e7a5
shardax <saradarails@gmail.com>
2020-05-18 23:18:34 -0400

    change to any resource

--- config/application.rb
+++ config/application.rb
@@ -22,11 +22,7 @@ module Unstoppable
     Rails.application.config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins  'http://localhost:3000'
-        resource '/sign_in',
-        methods: [:post],
-        headers: :any,
-        credentials: true
-        resource '*', headers: :any, methods: [:get, :post, :options], credentials: true
+        resource '*', headers: :any, methods: [:get, :post, :delete, :options], credentials: true
       end
     end
 
