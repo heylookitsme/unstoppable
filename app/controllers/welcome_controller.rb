1e93d84653eb43f49487611e09270acc562a3fdc
shardax <sarada.chintala@gmail.com>
2020-06-17 19:36:15 -0400

    Forward to new_user_session when user not logged in

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,19 +1,23 @@
 class WelcomeController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user! , :except => [:index]
 
   def index
-    if current_user.profile.step_status == Profile::STEP_CONFIRMED_EMAIL
-      redirect_to profiles_path
+    if current_user.blank?
+      redirect_to new_user_session_path
     else
-      case current_user.profile.step_status
-        when Profile::STEP_EMAIL_CONFIRMATION_SENT
-          redirect_to remind_confirmation_user_path(current_user)
-        when Profile::STEP_CANCER_HISTORY
-          redirect_to attachment_photo_path(:profile_id => current_user.profile.id)
-        when Profile::STEP_ABOUT_ME
-          redirect_to profile_build_path(:cancer_history, :profile_id => current_user.profile.id)
-        when Profile::STEP_BASIC_INFO
-          redirect_to profile_build_path(:about_me, :profile_id => current_user.profile.id)
+      if current_user.profile.step_status == Profile::STEP_CONFIRMED_EMAIL
+        redirect_to profiles_path
+      else
+        case current_user.profile.step_status
+          when Profile::STEP_EMAIL_CONFIRMATION_SENT
+            redirect_to remind_confirmation_user_path(current_user)
+          when Profile::STEP_CANCER_HISTORY
+            redirect_to attachment_photo_path(:profile_id => current_user.profile.id)
+          when Profile::STEP_ABOUT_ME
+            redirect_to profile_build_path(:cancer_history, :profile_id => current_user.profile.id)
+          when Profile::STEP_BASIC_INFO
+            redirect_to profile_build_path(:about_me, :profile_id => current_user.profile.id)
+        end
       end
     end
   end
