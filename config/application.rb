fcc77da645abd49e1da0d2567d03aa3e12d07cc3
shardax <sarada.chintala@gmail.com>
2020-07-31 11:07:20 -0400

    Change netlify url def

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,11 @@ module Unstoppable
 
     Rails.application.config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins  'http://localhost:3000', 'http://shardax-unstoppable-ui.netlify.app'
+        origins  'http://localhost:3000'
+        resource '*', headers: :any, methods: [:get, :post, :delete, :patch, :options], credentials: true
+      end
+      allow do
+        origins  'http://shardax-unstoppable-ui.netlify.app'
         resource '*', headers: :any, methods: [:get, :post, :delete, :patch, :options], credentials: true
       end
     end
