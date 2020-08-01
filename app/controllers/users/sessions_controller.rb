f81168dd2f216912592499ad817d02ae0899496a
shardax <sarada.chintala@gmail.com>
2020-08-01 15:49:13 -0400

    User appson of welcome controller

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -39,8 +39,8 @@ class Users::SessionsController < Devise::SessionsController
     Rails.logger.debug("request = #{request.format.inspect}")
     if request.format.json? #request.referrer.starts_with?("http://localhost:3000/login")
       Rails.logger.debug "Session Controller,redirecting to welcome_appjson_path current_user = #{current_user.inspect}"
-      #redirect_to welcome_appjson_path(:format => :json)
-      redirect_to appjson_user_path(:format => :json, :id => current_user.id)
+      redirect_to welcome_appjson_path(:format => :json)
+      #redirect_to appjson_user_path(:format => :json, :id => current_user.id)
       #respond_with resource, location: welcome_appjson_path(:format => :json)
     else
       unless current_user.blank?
