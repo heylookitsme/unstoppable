5b0a4374742f9b7a514cd9534e753d16fc999221
shardax <saradarails@gmail.com>
2020-12-03 18:06:18 -0500

    Retract back to defining uns1 domain

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,8 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
 if Rails.env == "production"
-  #Rails.application.config.session_store :cookie_store, key: "_RailsUserProfile_session",domain: "uns1.herokuapp.com"
-  Rails.application.config.session_store :cookie_store, key: "_RailsUserProfile_session", domain: "shardax-unstoppable-ui.netlify.app"
+  Rails.application.config.session_store :cookie_store, key: "_RailsUserProfile_session",domain: "uns1.herokuapp.com"
+  #Rails.application.config.session_store :cookie_store, key: "_RailsUserProfile_session", domain: "shardax-unstoppable-ui.netlify.app"
  
   #Rails.application.config.session_store :cookie_store, key: "_RailsUserProfile_session", domain: :all
 else
