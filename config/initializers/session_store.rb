421836d6312a982e33b9da2e824004d36a51c520
shardax <saradarails@gmail.com>
2020-12-03 17:59:41 -0500

    Attempt netlify domain

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -2,7 +2,9 @@
 
 if Rails.env == "production"
   #Rails.application.config.session_store :cookie_store, key: "_RailsUserProfile_session",domain: "uns1.herokuapp.com"
-  Rails.application.config.session_store :cookie_store, key: "_RailsUserProfile_session", domain: :all
+  Rails.application.config.session_store :cookie_store, key: "_RailsUserProfile_session", domain: "shardax-unstoppable-ui.netlify.app"
+ 
+  #Rails.application.config.session_store :cookie_store, key: "_RailsUserProfile_session", domain: :all
 else
   Rails.application.config.session_store :cookie_store, key: '_RailsUserProfile_session_dev'
 end
