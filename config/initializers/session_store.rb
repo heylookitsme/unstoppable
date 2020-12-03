618ac774914e1fa8aaa274d77bf58a2d4568dfa7
shardax <saradarails@gmail.com>
2020-12-03 15:55:07 -0500

    Add different cookie key name for development env

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -3,5 +3,5 @@
 if Rails.env == "production"
   Rails.application.config.session_store :cookie_store, key: "_RailsUserProfile_session",domain: "uns1.herokuapp.com"
 else
-  Rails.application.config.session_store :cookie_store, key: '_RailsUserProfile_session'
+  Rails.application.config.session_store :cookie_store, key: '_RailsUserProfile_session_dev'
 end
