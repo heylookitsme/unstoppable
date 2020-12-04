28f40c12b66211b0a16a7feb330e2e7325e4326c
shardax <saradarails@gmail.com>
2020-12-04 12:22:47 -0500

    Add same_site and secure params for cookie_store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
 if Rails.env == "production"
-  Rails.application.config.session_store :cookie_store, key: "_RailsUserProfile_session",domain: "uns1.herokuapp.com"
+  Rails.application.config.session_store :cookie_store, key: "_RailsUserProfile_session", domain: "uns1.herokuapp.com", same_site: :none, secure: true
   #Rails.application.config.session_store :cookie_store, key: "_RailsUserProfile_session", domain: "shardax-unstoppable-ui.netlify.app"
  
   #Rails.application.config.session_store :cookie_store, key: "_RailsUserProfile_session", domain: :all
