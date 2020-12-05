c3081614231b70a6cca373758a71ee31aad33635
shardax <saradarails@gmail.com>
2020-12-05 06:12:02 -0500

    cookie store, remove secure=true

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,10 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
 if Rails.env == "production"
-  Rails.application.config.session_store :cookie_store, key: "_RailsUserProfile_session", domain: "uns1.herokuapp.com", same_site: :none, secure: true
-  #Rails.application.config.session_store :cookie_store, key: "_RailsUserProfile_session", domain: "shardax-unstoppable-ui.netlify.app"
- 
-  #Rails.application.config.session_store :cookie_store, key: "_RailsUserProfile_session", domain: :all
+  Rails.application.config.session_store :cookie_store, key: "_RailsUserProfile_session", domain: "uns1.herokuapp.com", same_site: :none 
+  #, secure: true
 else
   Rails.application.config.session_store :cookie_store, key: '_RailsUserProfile_session_dev'
 end
