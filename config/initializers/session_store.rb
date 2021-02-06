fac81506d401b26311c7b6a043fb307f6c9fad7f
shardax <shardax2000@gmail.com>
2021-02-05 22:22:21 -0500

    test removal of secure=true

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,12 @@
 # Be sure to restart your server when you modify this file.
 
 if Rails.env == "production"
-  Rails.application.config.session_store :cookie_store, key: "_RailsUserProfile_session", domain: "uns1.herokuapp.com", same_site: :none, secure: true
+  #Rails.application.config.session_store :cookie_store, key: "_RailsUserProfile_session", domain: "uns1.herokuapp.com", same_site: :none, secure: true
+  Rails.application.config.session_store :cookie_store, key: "_RailsUserProfile_session", domain: "uns-production.herokuapp.com", same_site: :none#, secure: true
+elsif Rails.env == "staging"
+  Rails.application.config.session_store :cookie_store, key: "_RailsUserProfile_session", domain: "uns-staging.herokuapp.com", same_site: :none#, secure: true
+elsif Rails.env == "testing"
+  Rails.application.config.session_store :cookie_store, key: "_RailsUserProfile_session", domain: "uns-testing.herokuapp.com", same_site: :none#, secure: true
 else
   Rails.application.config.session_store :cookie_store, key: '_RailsUserProfile_session_dev'
 end
