d46eff7d28d77ac8c193a2c026c035a82e38605c
shardax <shardax2000@gmail.com>
2021-02-14 08:02:57 -0500

    Add logging in test env

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -51,7 +51,11 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
-
+  if ENV["RAILS_LOG_TO_STDOUT"].present?
+    logger           = ActiveSupport::Logger.new(STDOUT)
+    logger.formatter = config.log_formatter
+    config.logger    = ActiveSupport::TaggedLogging.new(logger)
+  end
 
   ActionMailer::Base.smtp_settings = {
   domain: 'gmail.com',
